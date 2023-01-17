581eb60b22189122f86df641817770d776660458
JenishLunagariya <jenishlunagariya71@gmail.com>
2023-01-17 18:23:49 +0530

    no update

--- DSA/KadaneAlgo.py
+++ DSA/KadaneAlgo.py
@@ -6,6 +6,6 @@ def kadane(arr):
         max_ending_here = max_ending_here + arr[i]
         if max_ending_here < -maxsize:
             max_ending_here = -maxsize
-        if max_ending_here > max_sofar:
+        elif max_ending_here > max_sofar:
             max_sofar  = max_ending_here
     return max_sofar
