0a926a9b9812287004b07b6a800d307e9a88e948
JenishLunagariya <jenishlunagariya71@gmail.com>
2023-02-27 18:04:23 +0530

    changes by django server

--- MyCart/shop/views.py
+++ MyCart/shop/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    # print("products",n)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    # print("nSlides",nSlides)
+    params = {'product':products,'no_of_slides':nSlides, 'range':range(1,nSlides)}
+    return render(request, 'shop/index.html',params)
 
 def about(request):
-    return HttpResponse("You are in About page")
+    return render(request,'shop/about.html')
 
 def contact(request):
     return HttpResponse("You are in Contact page")
