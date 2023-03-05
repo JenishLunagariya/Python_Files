0ffe95c9958e5f597226955f2ec732d51e30529b
JenishLunagariya <jenishlunagariya71@gmail.com>
2023-03-05 11:38:10 +0530

    updated import models

--- MyCart/shop/views.py
+++ MyCart/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Contact
 from math import ceil
 
 # Create your views here.
@@ -21,6 +21,13 @@ def about(request):
     return render(request,'shop/about.html')
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name','')
+        email = request.POST.get('email','')
+        phone = request.POST.get('phone','')
+        desc = request.POST.get('desc','')
+        contact = Contact(name=name,email=email,phone=phone,desc=desc)
+        contact.save()
     return render(request,'shop/contact.html')
 
 def tracker(request):
@@ -29,8 +36,10 @@ def tracker(request):
 def search(request):
     return render(request,'shop/search.html')
 
-def prodView(request):
-    return render(request,'shop/prodView.html')
+def prodView(request,id):
+    product = Product.objects.filter(id=id)
+    # print(product)
+    return render(request,'shop/prodView.html',{'product':product[0]})
 
 def checkout(request):
     return render(request,'shop/checkout.html')
